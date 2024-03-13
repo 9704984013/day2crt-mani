e5d2e7f659c8e1f40c56886057c70cc3c91239d9
9704984013 <162981101+9704984013@users.noreply.github.com>
2024-03-13 09:10:32 +0530

    Add files via upload

---
+++ day2pyd.py
@@ -0,0 +1,300 @@
+
+a,b = 7,5
+
+print(b,a)
+
+# a=5, b=7
+
+# # a=5, b=7
+# print(a,b)
+
+#Eg:2
+#a=a-b a=12
+#b=a-b  b=12-7
+#a=a-b a=12-5=7
+
+
+
+
+
+gyfy8f7gug7
+5545454444555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555
+#print(a, b)
+
+
+#a=a*b
+#b=a/b
+#a=a/b
+#print(a,b)
+
+# a, b=b,  a# only in python
+# print(a,b)
+
+# a=a*b #a=35
+# b=a/b #b=35/7 =5.0
+# a-a/b #a=35/5= 7.0
+# print(int(a),
+
+# id() --> used to find the memory address of variable
+# a = 7
+# b = 8
+#print(id(a))
+# print(od(b))
+
+# ---> keywords
+#keywords are reserved words which provides special mening to
+# compiler or interpretor
+
+ #import keyword
+# print(keyword.kwlist)
+# print (len(keyword.kwlist))
+#print (type(keyword.kwlist))
+
+#to Check  the string is keywordor not
+#print keyword.isdkeyword("mani")#
+
+
+# !---> literals
+# literal is the constant values assigned to a variables
+# A Variables is considers to be constant when it is defines
+#in caps
+
+# a= 78 # a is variable
+# A=78 # A is constant
+
+# hash()---> it creats a high value for constant datatypes and
+# provides error for non constant datatypes
+#  n1= 89+7j
+#print(hash(n1))
+
+# f1 =[7,8,9]
+# print(hash(f1)) #error---> list is non
+
+
+#b = 9
+# b = 90
+#print(id(a))
+# print(id(b))
+
+# ! ----> oprators
+# ? opeaartors are symobls which is used to perform various opearions
+# ? between 2 or moer operands
+
+# arithmatic
+# assignment
+# logical
+# relationl or comparison
+# bitwise
+# identity
+# membership
+
+# todo ----> arithmatic --> +,-,*,/,%,//,**
+# eg:1
+# a = 8
+# b = 6
+
+# b = 6
+# c = 9
+# print(a+b+c)
+
+# inpute() --> used to get the runtime input
+#n1 = input("Enter the value:")
+#print(type(n1))
+
+# a = 4
+# b = 2
+#  print(a/b) # is used to get the quotient value
+# print(a%b) # is used to get the remainder value
+
+# ! // ---> used for power of a number
+# a = 765433
+# b = 19
+# print(a/b) # --> the out will be in innteger & the outputwill be
+#based on floor value
+
+# !  ** ---> used for power of a number
+
+# ! comparison ---> ==,>,<,!=,<=,>=
+# a = 8
+# b= 7
+# print (a==b)
+
+# a = 9
+# b = 5
+# print(a==b)
+
+# ! bitwise operator ---> &,|,^,~,<<,>>
+# a = 7
+# b =  4
+# print(a&b)
+
+# 2^4 2^3 2^2 2^1 2^0
+# 8    4    2    1
+
+# 0   1     1   0   # ---> 7
+# 0   1     0   0   # ---> 4 &
+#--------------------------
+# 0   1     0   0
+
+#0   1   1   1   #-->7
+#0   1   0   0   #-->6&
+#---------------
+#0   1   1   1 #--> 7
+
+#~-->
+#a = 9876
+#print(~a)
+
+#a = 9
+
+# 128 64 32 16 8 4 2 1
+#  0   0  0  0 1 0 0 1 #-->9
+
+#  1   1  1  1 0 1 1 0 #-->-10
+
+#  0   0  0  0 1 0 0 1-->10
+
+#  1   1  1  1 0 1 --> 1s compliment of 10
+               # 1 --> 2s compliment
+#-----------------------------
+
+#      1  1  1  1  0 1 1 0 --> -10
+
+
+# 256 128 64 32 16 8 4 2 1
+#  0   0   0  0  0 0 1 0 1  3<<
+#  0   0   0  1  0 1 0 0 0 --> 40
+# 107
+
+# <<,>>
+# print(5>>1)
+# print(16>>4)
+
+ # ! Logical operator
+ # and, or, not
+
+#a = 6
+# print(a is > 3 and a is <10)
+# print(a>7 or a<30)
+# print(not (a>8 and a<10))
+
+
+ # ! Identity operator
+ #It is used to compare the memory location that the values are stored
+ # is, is not
+#a = 8
+#b = 8
+#print( a is b )
+#print(a==b)
+
+#a=[1, 2, 3]
+#b=[1, 2, 3]
+#print(a is b)
+
+ #a = [1, 2, 3]
+ #b = [1, 2, 3]
+ #print(a is not b)
+
+ # Membership Operator
+ # in, not in
+#l1 = [7, 8, 9, 0, 6, 5]
+#num = 55
+#print(num in l1)
+
+#l1 = [7, 8, 9, 0, 6, 5]
+#num = 55
+#print(num not in l1)
+
+ #num=678
+ #print(8 in num) #error
+
+ # ! 14 Conditional statement
+ # if, elif, else
+
+ #Eg;-1
+ #--> c syntax
+ # if (condition){
+ #     statement;
+ #     statement;
+ #     statement;
+ #}
+ #python syntax
+ # if condition:
+ #    statement;
+ #    statement;
+ #  statement;
+
+# Eg: 1
+#a = 6
+#if a:
+ #   print("hello")
+
+ #Eg:2
+ #a = 6
+ #if a >3:
+ #print("yes")
+ #else:
+ #print("no")
+ 
+#EG:3
+# if 7>8:
+#print("hello")
+
+#print("no")
+
+
+#Eg:4
+# a = 0
+# a = none
+# a = false
+# a = ""
+# if a:
+#   print("yes")
+#else:
+#   print("no") 
+
+
+# a number is even or odd
+# n = int(input("enter the number:  "))
+ #  if n = %2==0:
+  #      print(n, "is even")
+  #  else:
+   #    print(n, "is odd")
+
+  
+
+name =input("enter the name: ")
+age =int(input("enter the age: "))
+nationality=input("enter the nation: ")
+       
+if age>=18 and nationality=="Indian":
+        print("elagibility for vote")
+0else :
+        print("not eligible")
+    
+   
+  
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
